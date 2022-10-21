7647e1044e3d0c01aecf0e2232e2dc37604643de
Gill Lambrigts <gilllambrigts@icloud.com>
2022-10-21 15:17:51 +0200

    Initial Commit

---
+++ PogoEinstein/PogoEinsteinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PogoEinsteinApp.swift
+//  PogoEinstein
+//
+//  Created by Gill Lambrigts on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PogoEinsteinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
